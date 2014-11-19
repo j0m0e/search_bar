7d9fa96b49ce37bc54be18e3d313bbe648f65400
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-19 15:17:43 -0500

    ran migrations, schema loaded

--- search_bar_app/db/migrate/20141119172715_create_join_table_user_zip_code.rb
+++ search_bar_app/db/migrate/20141119172715_create_join_table_user_zip_code.rb
@@ -3,6 +3,6 @@ class CreateJoinTableUserZipCode < ActiveRecord::Migration
     create_join_table :users, :zip_codes 
       # t.index [:user_id, :zip_code_id]
       # t.index [:zip_code_id, :user_id]
-    end
+    
   end
 end
--- search_bar_app/db/migrate/20141119172741_create_join_table_bar_user.rb
+++ search_bar_app/db/migrate/20141119172741_create_join_table_bar_user.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableBarUser < ActiveRecord::Migration
   def change
-    create_join_table :bars, :users 
+    create_join_table :bars, :users
       # t.index [:bar_id, :user_id]
       # t.index [:user_id, :bar_id]
-    end
+    
   end
 end
