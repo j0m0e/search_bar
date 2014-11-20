8407a1f71fc497d173c9775fde6b80f43fc2cb6f
Rebecca Ann Wong <rebecca.ann.wong@gmail.com>
2014-11-20 15:31:57 -0500

    login logout

--- search_bar_app/app/controllers/users_controller.rb
+++ search_bar_app/app/controllers/users_controller.rb
@@ -7,14 +7,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to user_path
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
