e79f4ad21697871c3e220b164ed38111d1a8a620
Rebecca Ann Wong <rebecca.ann.wong@gmail.com>
2014-11-20 12:32:04 -0500

    routes

--- search_bar_app/spec/models/bar_spec.rb
+++ search_bar_app/spec/models/bar_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper' 
 
 describe Bar do
-	let(:bar) { Bar.new( business_id: "yelp-new-york")}
+	let(:yelpbar) { Bar.new( business_id: "yelp-bar-new-york")}
 	
   it "is valid with a business_id" do
-    expect(bar).to be_valid
+    expect(yelpbar).to be_valid
   end
 end
 
--- search_bar_app/spec/models/quizimages_spec.rb
+++ search_bar_app/spec/models/quizimages_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe QuizImg do
 	let(:image) { QuizImg.new( name: "photo1",
                          		 value: "1",
-                         		 filepath: "hello"
-                         		 category: )}
+                         		 filepath: ""
+                         		 category: "celebrity")}
 	
   it "is valid with a name, value, filepath and category" do
     expect(image).to be_valid
   end
 end
 
+# image path/ image
