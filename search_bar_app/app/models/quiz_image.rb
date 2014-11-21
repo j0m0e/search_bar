608e7b06d3a33b78851677a6cb72b126a83d4d15
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-21 11:41:42 -0500

    added seed data, controller, and view for category #2, tv_shows

--- search_bar_app/app/models/quiz_image.rb
+++ search_bar_app/app/models/quiz_image.rb
@@ -4,19 +4,19 @@ class QuizImage < ActiveRecord::Base
   validates :category, presence: true
 
 	def self.generate_category_1_image_array
-		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 4 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 8 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 12 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 16 ).sample)]).shuffle
 		return category_1_image_array
 	end
 
 	def self.generate_category_2_image_array
-		category_1_image_array = ([(QuizImage.where( :category => "drinks", :value => 1 ).sample),
-																(QuizImage.where( :category => "drinks", :value => 2 ).sample),
-																(QuizImage.where( :category => "drinks", :value => 3 ).sample),
-																(QuizImage.where( :category => "drinks", :value => 4 ).sample)]).shuffle
-		return category_1_image_array
+		category_2_image_array = ([(QuizImage.where( :category => "tv_show", :value => 4 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 8 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 12 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 16 ).sample)]).shuffle
+		return category_2_image_array
 	end
 
 	def self.generate_category_3_image_array
--- search_bar_app/db/seeds.rb
+++ search_bar_app/db/seeds.rb
@@ -3,44 +3,123 @@ QuizImage.delete_all
 quiz_images = QuizImage.create([ 
 												{ filepath: "beat_up_chuck_taylor.jpg", 
 													category: "clothing", 
-													value: "1" },
+													value: "4" },
 
 													{ filepath: "sperry_topsider.jpg", 
 													category: "clothing", 
-													value: "3" },
+													value: "12" },
 
 													{ filepath: "birkin_bag.jpg", 
 													category: "clothing", 
-													value: "4" },
+													value: "16" },
 
 													{ filepath: "vans_sneakers.jpg", 
 													category: "clothing", 
-													value: "2" },
+													value: "8" },
 
 													{ filepath: "bow_tie.jpg", 
 													category: "clothing", 
-													value: "4" },
+													value: "16" },
 
 													{ filepath: "flannel_shirt.jpg", 
 													category: "clothing", 
-													value: "2" },
+													value: "8" },
 
 													{ filepath: "flip_flops.jpg", 
 													category: "clothing", 
-													value: "2" },
+													value: "8" },
 
 													{ filepath: "jean_jacket.jpg", 
 													category: "clothing", 
-													value: "2" },
+													value: "8" },
 
 													{ filepath: "leather_jacket.jpg", 
 													category: "clothing", 
-													value: "1" },
+													value: "4" },
 
 													{ filepath: "overalls.jpg", 
 													category: "clothing", 
-													value: "1" }
+													value: "4" },
+############################################################
+													{ filepath: "suits.jpg", 
+													category: "tv_show", 
+													value: "16" },
 
+													{ filepath: "downton_abbey.jpg", 
+													category: "tv_show", 
+													value: "16" },
+
+													{ filepath: "madmen.jpg", 
+													category: "tv_show", 
+													value: "16" },
+
+													{ filepath: "house_of_cards.jpg", 
+													category: "tv_show", 
+													value: "16" },
+############################################################
+													{ filepath: "sherlock.jpg", 
+													category: "tv_show", 
+													value: "12" },
+
+													{ filepath: "how_to_get_away_with_murder.jpg", 
+													category: "tv_show", 
+													value: "12" },
+
+													{ filepath: "scandal.png", 
+													category: "tv_show", 
+													value: "12" },
+
+													{ filepath: "greys_anatomy.jpg", 
+													category: "tv_show", 
+													value: "12" },
+
+													{ filepath: "dexter.jpg", 
+													category: "tv_show", 
+													value: "12" },
+
+													{ filepath: "game_of_thrones.jpg", 
+													category: "tv_show", 
+													value: "12" },
+############################################################
+													{ filepath: "friends.jpg", 
+													category: "tv_show", 
+													value: "8" },
+
+													{ filepath: "how_i_met-your_mother.jpg", 
+													category: "tv_show", 
+													value: "8" },
+
+													{ filepath: "seinfeld.jpg", 
+													category: "tv_show", 
+													value: "8" },
+
+													{ filepath: "arrested_development.jpg", 
+													category: "tv_show", 
+													value: "8" },
+
+													{ filepath: "true_detective.jpg", 
+													category: "tv_show", 
+													value: "8" },
+############################################################
+													{ filepath: "breaking_bad.jpg", 
+													category: "tv_show", 
+													value: "4" },
+
+													{ filepath: "walking_dead.jpg", 
+													category: "tv_show", 
+													value: "4" },
+
+													{ filepath: "duck_dynasty.jpg", 
+													category: "tv_show", 
+													value: "4" },
+
+													{ filepath: "deadliest_catch.jpg", 
+													category: "tv_show", 
+													value: "4" },
+
+													{ filepath: "always_sunny.jpg", 
+													category: "tv_show", 
+													value: "4" }
 													])
 
 
