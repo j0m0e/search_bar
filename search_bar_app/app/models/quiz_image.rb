e42e1d6df3fa5f11d37320fdb713d383fb4dad70
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-21 09:18:52 -0500

    more refactoring of the quiz_image controller and method. filler for testing still in place.

--- search_bar_app/app/models/quiz_image.rb
+++ search_bar_app/app/models/quiz_image.rb
@@ -4,5 +4,37 @@ class QuizImage < ActiveRecord::Base
   validates :filepath, presence: true, uniqueness: true
   validates :category, presence: true
 
+	def self.generate_category_1_image_array
+		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+		return category_1_image_array
+	end
+
+	def self.generate_category_2_image_array
+		category_1_image_array = ([(QuizImage.where( :category => "drinks", :value => 1 ).sample),
+																(QuizImage.where( :category => "drinks", :value => 2 ).sample),
+																(QuizImage.where( :category => "drinks", :value => 3 ).sample),
+																(QuizImage.where( :category => "drinks", :value => 4 ).sample)]).shuffle
+		return category_1_image_array
+	end
+
+	def self.generate_category_3_image_array
+		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+		return category_1_image_array
+	end
+
+	def self.generate_category_4_image_array
+		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+		return category_1_image_array
+	end
+
 
 end
