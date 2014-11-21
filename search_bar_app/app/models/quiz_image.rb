603873d4f11d7ff2f008a87d74483968afb05afd
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-21 13:40:00 -0500

    updated seed file

--- search_bar_app/app/models/quiz_image.rb
+++ search_bar_app/app/models/quiz_image.rb
@@ -4,34 +4,34 @@ class QuizImage < ActiveRecord::Base
   validates :category, presence: true
 
 	def self.generate_category_1_image_array
-		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 4 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 8 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 12 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 16 ).sample)]).shuffle
+		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 34 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 67 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 100 ).sample)]).shuffle
 		return category_1_image_array
 	end
 
 	def self.generate_category_2_image_array
-		category_2_image_array = ([(QuizImage.where( :category => "tv_show", :value => 4 ).sample),
-																(QuizImage.where( :category => "tv_show", :value => 8 ).sample),
-																(QuizImage.where( :category => "tv_show", :value => 12 ).sample),
-																(QuizImage.where( :category => "tv_show", :value => 16 ).sample)]).shuffle
+		category_2_image_array = ([(QuizImage.where( :category => "tv_show", :value => 1 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 34 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 67 ).sample),
+																(QuizImage.where( :category => "tv_show", :value => 100 ).sample)]).shuffle
 		return category_2_image_array
 	end
 
 	def self.generate_category_3_image_array
 		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+																(QuizImage.where( :category => "clothing", :value => 34 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 67 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 100 ).sample)]).shuffle
 		return category_1_image_array
 	end
 
 	def self.generate_category_4_image_array
 		category_1_image_array = ([(QuizImage.where( :category => "clothing", :value => 1 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 2 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 3 ).sample),
-																(QuizImage.where( :category => "clothing", :value => 4 ).sample)]).shuffle
+																(QuizImage.where( :category => "clothing", :value => 34 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 67 ).sample),
+																(QuizImage.where( :category => "clothing", :value => 100 ).sample)]).shuffle
 		return category_1_image_array
 	end
 
