28216c010ff9359cf7b2a757602dac50bc0cbbeb
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-21 18:31:02 -0500

    started views/bars/results. render populates with infor from search api.

--- search_bar_app/app/controllers/bars_controller.rb
+++ search_bar_app/app/controllers/bars_controller.rb
@@ -2,13 +2,12 @@ class BarsController < ApplicationController
 
 	def search
 		session[:zip_code] = params[:zip_code]
-		@search_results	= SearchAPI.search_for_bar("dive bar", session[:zip_code])
-		binding.pry
 	end	
 
 
 	def results	
-		
+		@search_results	= SearchAPI.search_for_bar("divebars", session[:zip_code])
+	
 	end
 
 
