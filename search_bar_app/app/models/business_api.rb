8a71875d3ac5357e21e497553ed623343129ff9f
j0m0e <jamieyerushalmi@gmail.com>
2014-11-21 16:23:27 -0500

    added business api methods to BusinessAPI model

--- search_bar_app/app/models/business_api.rb
+++ search_bar_app/app/models/business_api.rb
@@ -1,8 +1,23 @@
 class BusinessAPI
 
-# method that searches by business id
 
-# method that returns results based on that search 
+	
+	# makes client object which allows access to yelp api
+	def self.make_client
+		client = Yelp::Client.new({ consumer_key: 'dZJPaWyOc0QI2LV0_IiFgw' ,
+                            consumer_secret: 'pSH96gUah8_5trIEnuWMENKiiP0',
+                            token: '4RvQDhd44HhAJdefTKeW3rmwGjs5MbG-',
+                            token_secret:'6tkuLz4ppLJrhiPou8SBbMy-E_8'
+                          })
+	end
+
+	#retrieves business info by business id
+	def self.retrieve_business(business_id)
+		client = make_client
+		business = client.business(business_id).to_json
+		parsed_business = JSON.parse(business)
+	end
+
 
 
 end
