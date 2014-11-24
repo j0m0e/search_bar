6140ec3f2184a0f546408e3de81160d52f37bf10
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-24 14:12:57 -0500

    added self.quiz_results method to quiz_images model

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,12 +1,14 @@
 class BarsController < ApplicationController
 
 	def search 
+		redirect_to "/quiz_images/1"
 		session[:zip_code] = params[:zip_code]
-		if session[:current_user_id]
-			@user = User.find(session[:current_user_id])
-		end
-		@search_results	= SearchAPI.search_for_bar("divebars", session[:zip_code])
-		render :results
+		QuizImage.quiz_results(session[:value_array])
+		@search_results	= SearchAPI.search_for_bar(@quiz_results, session[:zip_code])
 	end	
 
 end
+
+		# if session[:current_user_id]
+		# 	@user = User.find(session[:current_user_id])
+		# end
--- app/models/quiz_image.rb
+++ app/models/quiz_image.rb
@@ -35,5 +35,22 @@ class QuizImage < ActiveRecord::Base
 		return category_1_image_array
 	end
 
+	def self.quiz_results(value_array)
+		quiz_total = (((value_array).reduce(:+))/2).round
+		if quiz_total.between?(1,25)
+			@quiz_results = "divebars,poolhalls"
+		elsif quiz_total.between?(26,50)
+			@quiz_results = "pubs,sportsbars,irish_pubs"
+		elsif quiz_total.between?(51,75)
+			@quiz_results = "wine_bars,pianobars,lounges"
+		elsif quiz_total.between?(76,100)
+			@quiz_results = "champagne_bars,cocktailbars"	
+		end		
+		binding.pry	
+	end
+	
+
+
+
 
 end
