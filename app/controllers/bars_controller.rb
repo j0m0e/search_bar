246bf6b5a3b1fb8f370ecc31bf5ddd8e93158ca1
j0m0e <jamieyerushalmi@gmail.com>
2014-11-24 17:29:32 -0500

    fixed results method in quizimages controller

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,13 +1,14 @@
 class BarsController < ApplicationController
 
 	def search 
+		session[:zip_code] = params[:zip_code]
 		redirect_to "/quiz_images/1"
 	end	
 
 	def results
-		QuizImage.quiz_results(session[:value_array])
-		@search_results = SearchAPI.search_for_bar(@quiz_results, session[:zip_code])
-		binding.pry
+		find_quiz_results = QuizImage.quiz_results(session[:value_array])
+		@search_results = SearchAPI.search_for_bar(find_quiz_results, session[:zip_code])
+
 	end	
 
 end
