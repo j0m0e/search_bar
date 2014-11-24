ec86f5c4e41326ec1426cb876efec48f1731dae6
j0m0e <jamieyerushalmi@gmail.com>
2014-11-23 21:38:46 -0500

    added comments to search api model

--- search_bar_app/app/models/search_api.rb
+++ search_bar_app/app/models/search_api.rb
@@ -1,32 +1,30 @@
 class SearchAPI 
-# Method that searches by: 
-# Zipcode
+
 # Category_Filter: Formal to Casual
-# Open today
-# 3 star rating and above
-# Sort by best match or highest rated
-# Need a term in method - (bar)
-# method that returns results based on that search 
+
+
 	
+	# allows access to Yelp API
 	def self.make_client
-	client = Yelp::Client.new({ consumer_key: 'dZJPaWyOc0QI2LV0_IiFgw' ,
+		client = Yelp::Client.new({ consumer_key: 'dZJPaWyOc0QI2LV0_IiFgw' ,
                             consumer_secret: 'pSH96gUah8_5trIEnuWMENKiiP0',
                             token: '4RvQDhd44HhAJdefTKeW3rmwGjs5MbG-',
                             token_secret:'6tkuLz4ppLJrhiPou8SBbMy-E_8'
                           })
 	end
 
+	# searches Yelp API for 3 bars specified by quiz results and zipcode. Returns best match by default.
 	def self.search_for_bar(quiz_results, zip_code) 
-	parameters = { term: "bars",
+		parameters = { term: "bars",
 								 category_filter: quiz_results,
 								 location: zip_code,
 								 limit: 3}
-	client = make_client
-	results = client.search(zip_code, parameters).to_json
-	parsed_results = JSON.parse(results)
+		client = make_client
+		results = client.search(zip_code, parameters).to_json
+		parsed_results = JSON.parse(results)
 	end
 
-# returns 3 results in an array of JSON objects
+
 
 end
 
