4f4c0d851ad3dd480c2418458aeeac60cda277c7
RossWittenberg <ross.wittenberg@gmail.com>
2014-11-25 10:19:04 -0500

    rerouted user login to index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
+    
   end
 
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:current_user_id] = user.id
-      redirect_to user_path(:id)
+      redirect_to '/'
     else
       redirect_to login_path
     end
